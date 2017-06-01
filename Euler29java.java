938f6934208c719f887b327201569fbd50a2ba28
gclark14 <unrivalled.dh@gmail.com>
2017-05-31 20:05:58 -0700

    Add files via upload

---
+++ Euler29java.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package euler29java;
+
+/**
+ *
+ * @author gabriel
+ */
+public class Euler29java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DistinctPowers dp = new DistinctPowers();
+    }
+    
+}
